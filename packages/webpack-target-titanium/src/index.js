7bfd8c12f821bd4e1f9b3d1cd12bd28a01abc8e3
Jan Vennemann <jan.vennemann@gmx.net>
2018-06-13 22:40:15 +0300

    feat: platform aware filesystem plugin (#6)
    Expose a new plugin that allows Webpack to import platform specific files if available. Platform specific files contain the platform name as a suffix, e.g. util.android.js.

--- packages/webpack-target-titanium/src/index.js
+++ packages/webpack-target-titanium/src/index.js
@@ -1,9 +1,15 @@
+'use strict';
+
 const GenerateAppJsPlugin = require('./plugins/GenerateAppJsPlugin');
+const PlatformAwareFileSystem = require('./PlatformAwareFileSystem');
+const PlatformAwareFileSystemPlugin = require('./plugins/PlatformAwareFileSystemPlugin');
 const titaniumTarget = require('./titanium-target');
 const WatchStateNotifierPlugin = require('./plugins/WatchStateNotifierPlugin');
 
 module.exports = {
 	GenerateAppJsPlugin,
+	PlatformAwareFileSystem,
+	PlatformAwareFileSystemPlugin,
 	titaniumTarget,
 	WatchStateNotifierPlugin
 };

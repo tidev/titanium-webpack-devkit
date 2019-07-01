9b0f1484a9b18b3fbdc2a51975fa9df97c5dffd6
Jan Vennemann <jan.vennemann@gmx.net>
2019-07-01 17:11:45 +0200

    chore: delete unused index.js (#20)

--- index.js
+++
@@ -1,9 +0,0 @@
-const GenerateAppJsPlugin = require('./lib/plugins/GenerateAppJsPlugin');
-const titaniumTarget = require('./lib/titanium-target');
-const WatchStateNotifierPlugin = require('./lib/plugins/WatchStateNotifierPlugin');
-
-module.exports = {
-	GenerateAppJsPlugin,
-	titaniumTarget,
-	WatchStateNotifierPlugin
-};

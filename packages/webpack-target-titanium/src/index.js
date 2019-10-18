100ade0ea60db4c504b8f52c20b24d2501fe0a5d
Jan Vennemann <jan.vennemann@gmx.net>
2019-10-18 15:48:55 +0200

    feat: titanium externals plugin
    automatically marks used modules as external so webpack won't try to process them

--- packages/webpack-target-titanium/src/index.js
+++ packages/webpack-target-titanium/src/index.js
@@ -3,9 +3,11 @@
 const GenerateAppJsPlugin = require('./plugins/GenerateAppJsPlugin');
 const PlatformResolverPlugin = require('./plugins/PlatformResolverPlugin');
 const titaniumTarget = require('./titanium-target');
+const TitaniumExternalsPlugins = require('./plugins/TitaniumExternalsPlugin');
 
 module.exports = {
 	GenerateAppJsPlugin,
 	PlatformResolverPlugin,
-	titaniumTarget
+	titaniumTarget,
+	TitaniumExternalsPlugins
 };

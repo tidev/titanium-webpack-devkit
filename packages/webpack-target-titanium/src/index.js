c910e341fdc52a60e8efde4887d2a0d5b678fd6c
Jan Vennemann <jan.vennemann@gmx.net>
2019-10-18 15:38:19 +0200

    refactor!: remove unused state notifier plugin

--- packages/webpack-target-titanium/src/index.js
+++ packages/webpack-target-titanium/src/index.js
@@ -3,11 +3,9 @@
 const GenerateAppJsPlugin = require('./plugins/GenerateAppJsPlugin');
 const PlatformResolverPlugin = require('./plugins/PlatformResolverPlugin');
 const titaniumTarget = require('./titanium-target');
-const WatchStateNotifierPlugin = require('./plugins/WatchStateNotifierPlugin');
 
 module.exports = {
 	GenerateAppJsPlugin,
 	PlatformResolverPlugin,
-	titaniumTarget,
-	WatchStateNotifierPlugin
+	titaniumTarget
 };

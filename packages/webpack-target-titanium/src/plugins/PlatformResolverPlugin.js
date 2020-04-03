283a59d333c81c816dcfb64ea7a66d2a95b646c9
Jan Vennemann <jan.vennemann@gmx.net>
2020-04-03 18:31:06 +0200

    fix: respect existing extension
    BREAKING CHANGE: Inserts new `ignoreExtension` argument to plugin constructor. You need to create and add two plugin instances with altering `ignoreExtension` values.

--- packages/webpack-target-titanium/src/plugins/PlatformResolverPlugin.js
+++ packages/webpack-target-titanium/src/plugins/PlatformResolverPlugin.js
@@ -1,7 +1,10 @@
+const path = require('path');
+
 class PlatformResolverPlugin {
-	constructor(source, platform, target) {
+	constructor(source, platform, ignoreExtension, target) {
 		this.source = source;
 		this.target = target;
+		this.ignoreExtension = ignoreExtension;
 		this.platform = platform;
 	}
 
@@ -10,10 +13,21 @@ class PlatformResolverPlugin {
 		resolver
 			.getHook(this.source)
 			.tapAsync('PlatformResolverPlugin', (request, resolveContext, callback) => {
+				let platformPath;
+				let relativePath;
+				const ext = path.extname(request.path);
+				if (!this.ignoreExtension && ext.length) {
+					const platformSuffix = `.${this.platform}${ext}`;
+					platformPath = request.path.slice(0, -ext.length) + platformSuffix;
+					relativePath = request.relativePath && request.relativePath.slice(0, -ext.length) + platformSuffix;
+				} else {
+					platformPath = request.path + `.${this.platform}`;
+					relativePath = request.relativePath && request.relativePath + `.${this.platform}`;
+				}
 				const platformRequest = {
 					...request,
-					path: request.path + `.${this.platform}`,
-					relativePath: request.relativePath && request.relativePath + `.${this.platform}`
+					path: platformPath,
+					relativePath
 				};
 				resolver.doResolve(target, platformRequest, null, resolveContext, callback);
 			});
